0ca6d9db359a3f254168ffd179d75d53e612d363
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2023-05-31 19:15:21 -0400

    feat(scripts): add deployment scripts for contracts incl blueprint

---
+++ scripts/deploy.py
@@ -0,0 +1,75 @@
+import click
+from datetime import time
+from ape import project, convert, networks
+from ape.cli import NetworkBoundCommand, network_option, account_option, ape_cli_context
+from ape.types import AddressType, HexBytes
+
+try:
+    from ape_tokens import tokens as token_lookup
+except ImportError:
+    token_lookup = {}
+
+
+@click.group()
+def cli():
+    """
+    Deploy contract from project
+    """
+
+
+@cli.command(cls=NetworkBoundCommand, short_help="Deploy the StreamFactory contract")
+@account_option()
+@network_option()
+@ape_cli_context()
+@click.option("--blueprint", default=None)
+def stream_factory(cli_ctx, account, network, blueprint):
+    if not blueprint:
+        blueprint_bytecode = b"\xFE\x71\x00" + HexBytes(  # ERC5202 preamble
+            project.StreamManager.contract_type.deployment_bytecode.bytecode
+        )
+        # the length of the deployed code in bytes
+        len_bytes = len(blueprint_bytecode).to_bytes(2, "big")
+        blueprint = account.call(
+            networks.ecosystem.create_transaction(
+                # copy <blueprint_bytecode> to memory and `RETURN` it per EVM creation semantics
+                # PUSH2 <len> RETURNDATASIZE DUP2 PUSH1 10 RETURNDATASIZE CODECOPY RETURN
+                data=b"\x61"
+                + len_bytes
+                + b"\x3d\x81\x60\x0a\x3d\x39\xf3"
+                + blueprint_bytecode
+            )
+        ).contract_address
+        cli_ctx.logger.success(
+            f"Blueprint 'StreamManager' deployed to: {click.style(blueprint, bold=True)}"
+        )
+
+    account.deploy(project.StreamFactory, blueprint, publish=click.confirm("Publish"))
+
+
+@cli.command(cls=NetworkBoundCommand, short_help="Deploy the StreamManager contract")
+@account_option()
+@network_option()
+@click.option("--owner", default=None)
+@click.option("--min-stream-life", type=int, default=60 * 60)
+@click.argument("tokens", nargs=-1)
+def stream_manager(account, network, owner, min_stream_life, tokens):
+    if len(tokens) > 20:
+        raise click.BadArgumentUsage("Doesn't accept more than 20 tokens")
+
+    if len(set(tokens)) < len(tokens):
+        raise click.BadArgumentUsage("Duplicate in accepted tokens")
+
+    token_addresses = []
+    for token in tokens:
+        try:
+            token_addresses.append(token_lookup[token])  # allow specifying by address
+        except KeyError:
+            token_addresses.append(token)
+
+    account.deploy(
+        project.StreamManager,
+        owner or account,
+        min_stream_life,
+        token_addresses,
+        publish=click.confirm("Publish"),
+    )
