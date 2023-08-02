fa9efc2daba27dd3d2071696062c32e7379fd84f
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2023-08-02 15:59:31 -0400

    feat: add general purpose stream manager UI

---
+++ ui/app/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "flex-end",
+        padding: 12,
+      }}
+    >
+      <ConnectButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ ui/app/src/main.tsx
@@ -0,0 +1,38 @@
+import "./polyfills";
+import "./global.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { polygon, optimism, arbitrum, sepolia } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const { chains, publicClient } = configureChains(
+  // NOTE: Testnet deployment on Sepolia
+  [arbitrum, optimism, polygon, sepolia],
+  [publicProvider()],
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "ApePay Dashboard",
+  projectId: "YOUR_PROJECT_ID",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  </React.StrictMode>,
+);
