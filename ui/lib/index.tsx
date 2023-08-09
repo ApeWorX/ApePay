cbfa5c187950c9ae4e873012d42e72808fb866a9
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2023-08-08 22:32:13 -0400

    feat(react): add Stream Status display

--- ui/lib/index.tsx
+++ ui/lib/index.tsx
@@ -2,3 +2,4 @@ export { default as CreateStream } from "./CreateStream";
 export { default as UpdateStream } from "./UpdateStream";
 export { default as CancelStream } from "./CancelStream";
 export { default as StreamItem } from "./StreamItem";
+export { default as StreamStatus } from "./StreamStatus";
