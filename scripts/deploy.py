00fbbc2f2917de8303f4c5548b6b30cda13d9916
El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>
2023-08-15 17:48:07 -0400

    feat(contracts): deploy factory via create2 [SBK-211] (#31)
    * refactor: use new `.declare` function
    
    * feat: deploy StreamFactory using create2
    
    * fix: use Paris rules to avoid getting rugged by PUSH0
    
    * feat: allow deploying StreamManager via StreamFactory
    
    * fix: avoid doing address info lookup for None
    
    * refactor: use publish flag instead of interactive confirm
    
    * chore: add create2 deployments

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,7 +1,9 @@
 import click
-from ape import networks, project
+from ape import networks, project, Contract
 from ape.cli import NetworkBoundCommand, account_option, ape_cli_context, network_option
 from ape.types import HexBytes
+from ape.exceptions import ApeException
+from ape_ethereum.ecosystem import keccak
 
 try:
     from ape_tokens import tokens as token_lookup
@@ -21,38 +23,76 @@ def cli():
 @network_option()
 @ape_cli_context()
 @click.option("--blueprint", default=None)
-def factory(cli_ctx, account, network, blueprint):
-    if not blueprint:
-        blueprint_bytecode = b"\xFE\x71\x00" + HexBytes(  # ERC5202 preamble
-            project.StreamManager.contract_type.deployment_bytecode.bytecode
-        )
-        # the length of the deployed code in bytes
-        len_bytes = len(blueprint_bytecode).to_bytes(2, "big")
-        blueprint = account.call(
-            networks.ecosystem.create_transaction(
-                # copy <blueprint_bytecode> to memory and `RETURN` it per EVM creation semantics
-                # PUSH2 <len> RETURNDATASIZE DUP2 PUSH1 10 RETURNDATASIZE CODECOPY RETURN
-                data=b"\x61"
-                + len_bytes
-                + b"\x3d\x81\x60\x0a\x3d\x39\xf3"
-                + blueprint_bytecode
-            )
-        ).contract_address
-        cli_ctx.logger.success(
-            f"Blueprint 'StreamManager' deployed to: {click.style(blueprint, bold=True)}"
-        )
+@click.option("--create2", default=None, help="A string tag for the create2 deployment salt")
+@click.option("--publish", is_flag=True)
+def factory(cli_ctx, account, network, blueprint, create2, publish):
+    if create2:
+        # NOTE: This is the deployment address listed on the create2 deployer's github:
+        # https://github.com/pcaversaccio/create2deployer/tree/main#deployments-create2deployer
+        # TODO: Add SDK for create2 deployer
+        create2_deployer = Contract("0x13b0D85CcB8bf860b6b79AF3029fCA081AE9beF2")
+        salt = keccak(text=create2)
 
-    account.deploy(project.StreamFactory, blueprint, publish=click.confirm("Publish"))
+        if not blueprint:
+            blueprint_initcode = cli_ctx.provider.network.ecosystem.encode_contract_blueprint(
+                project.StreamManager.contract_type
+            ).data
+            blueprint = create2_deployer.computeAddress(salt, keccak(blueprint_initcode))
+            if not click.confirm(f"Deploy StreamManager blueprint to '{blueprint}'"):
+                return  # user abort
+
+            create2_deployer.deploy(
+                0,  # msg.value
+                salt,
+                blueprint_initcode,
+                sender=account,
+            )
+            cli_ctx.logger.success(
+                f"Blueprint 'StreamManager' deployed to: {click.style(blueprint, bold=True)}"
+            )
+            cli_ctx.chain_manager.contracts.cache_blueprint(
+                blueprint, project.StreamManager.contract_type
+            )
+
+        factory_initcode = project.StreamFactory.constructor.serialize_transaction(blueprint).data
+        factory_address = create2_deployer.computeAddress(salt, keccak(factory_initcode))
+
+        if click.confirm(f"Deploy StreamFactory to '{factory_address}'"):
+            create2_deployer.deploy(
+                0,  # msg.value
+                salt,
+                factory_initcode,
+                sender=account,
+            )
+            factory = project.StreamFactory.at(factory_address)
+
+            if publish:
+                cli_ctx.project_manager.track_deployment(factory)
+                cli_ctx.provider.network.publish_contract(factory.address)
+
+    else:
+        if not blueprint:
+            blueprint = project.StreamFactory.declare(sender=account).contract_address
+            cli_ctx.logger.success(
+                f"Blueprint 'StreamManager' deployed to: {click.style(blueprint, bold=True)}"
+            )
+
+        account.deploy(project.StreamFactory, blueprint, publish=publish)
 
 
 @cli.command(cls=NetworkBoundCommand, short_help="Deploy the StreamManager contract")
+@ape_cli_context()
 @account_option()
 @network_option()
+@click.option("--factory", default=None)
 @click.option("--owner", default=None)
 @click.option("--min-stream-life", type=int, default=60 * 60)
 @click.option("--validator", "validators", multiple=True, default=[])
+@click.option("--publish", is_flag=True)
 @click.argument("tokens", nargs=-1)
-def manager(account, network, owner, min_stream_life, validators, tokens):
+def manager(
+    cli_ctx, account, network, factory, owner, min_stream_life, validators, publish, tokens
+):
     if len(tokens) > 20:
         raise click.BadArgumentUsage("Doesn't accept more than 20 tokens")
 
@@ -66,11 +106,31 @@ def manager(account, network, owner, min_stream_life, validators, tokens):
         except KeyError:
             token_addresses.append(token)
 
-    account.deploy(
-        project.StreamManager,
-        owner or account,
-        min_stream_life,
-        list(validators),
-        token_addresses,
-        publish=click.confirm("Publish"),
-    )
+    if factory := factory and project.StreamFactory.at(factory):
+        if owner:
+            raise click.BadArgumentUsage("Cannot use 'owner' with 'factory'")
+
+        if min_stream_life != 60 * 60:
+            raise click.BadArgumentUsage("Cannot use custom 'min_stream_life'")
+
+        tx = factory.create(list(validators), token_addresses, sender=account)
+        try:
+            manager = project.StreamManager.at(tx.return_value)
+        except ApeException:
+            manager = project.StreamManager.at(factory.deployments(account))
+
+        cli_ctx.logger.success(f"StreamManager deployed to '{manager.address}'.")
+
+        if publish:
+            cli_ctx.project_manager.track_deployment(manager)
+            cli_ctx.provider.network.publish_contract(manager.address)
+
+    else:
+        account.deploy(
+            project.StreamManager,
+            owner or account,
+            min_stream_life,
+            list(validators),
+            token_addresses,
+            publish=publish,
+        )
