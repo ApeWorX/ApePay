9c48ec5425999a9186d0cdbc4c3c73eeab33430d
alexisnsns <alexisns@pm.me>
2023-09-27 16:54:36 -0400

    fix: edit other types to pass tests

--- ui/lib/CreateStream.tsx
+++ ui/lib/CreateStream.tsx
@@ -197,7 +197,12 @@ const CreateStream = (props: CreateStreamProps) => {
   const [selectedToken, setSelectedToken] = useState(
     "0x7F5c764cBc14f9669B88837ca1490cCa17c31607"
   );
-  const [tokens, setTokens] = useState([]);
+
+  type Token = {
+    address: string;
+    symbol: string;
+  };
+  const [tokens, setTokens] = useState<Token[]>([]);
   const [currentStep, setCurrentStep] = useState(0);
 
   // Fetch tokens from JSON
