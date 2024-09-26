68c0212ff9a7952fcc9795ba2d7c856ce496ff82
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2024-09-25 21:19:50 -0400

    test: add some extra checks

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -7,7 +7,7 @@ from eth_utils import to_int
 from apepay import exceptions as apepay_exc
 
 
-def test_create_stream(payer, token, create_stream, products):
+def test_create_stream(chain, payer, token, create_stream, products):
     with pytest.raises(apepay_exc.NotEnoughAllowance):
         create_stream(allowance=0)
 
@@ -15,15 +15,35 @@ def test_create_stream(payer, token, create_stream, products):
         create_stream(amount=token.balanceOf(payer) + 1)
 
     amount_per_second = sum(map(to_int, products))
-
     stream = create_stream()
+    total_funded = stream.info.funded_amount
 
+    assert stream.id == 0  # Sanity check that isolation is working
     assert stream.token == token
-    assert stream.id == 0
     assert stream.owner == payer
     assert stream.amount_per_second == amount_per_second
     assert stream.products == products
-    assert stream.time_left == timedelta(seconds=stream.amount_refundable // amount_per_second)
+
+    assert stream.is_active is True
+    assert stream.amount_claimable == 0
+    assert stream.amount_refundable == total_funded
+    assert stream.time_left == timedelta(seconds=total_funded // amount_per_second)
+
+    # Mine to the end of the stream
+    chain.mine(deltatime=int(stream.time_left.total_seconds()))
+
+    assert stream.is_active is False
+    assert stream.amount_claimable == total_funded
+    assert stream.amount_refundable == 0
+    assert stream.time_left == timedelta(seconds=0)
+
+    # Double check that if you call this at some point after the end, nothing changes
+    chain.mine(deltatime=60 * 60)
+
+    assert stream.is_active is False
+    assert stream.amount_claimable == total_funded
+    assert stream.amount_refundable == 0
+    assert stream.time_left == timedelta(seconds=0)
 
 
 def test_cancel_stream(chain, token, payer, starting_balance, controller, MIN_STREAM_LIFE, stream):
