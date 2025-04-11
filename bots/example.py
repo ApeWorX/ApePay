8a0383d880f7366f6d8ffdc8bd1028bb763c9076
Doggie B <3859395+fubuloubu@users.noreply.github.com>
2025-04-11 17:49:38 -0400

    fix(example): upgrade example bot to latest SDK

--- bots/example.py
+++ bots/example.py
@@ -1,10 +1,10 @@
 import os
 
-from silverback import SilverbackApp
+from silverback import SilverbackBot
 
 from apepay import Stream, StreamManager
 
-app = SilverbackApp()
+bot = SilverbackBot()
 # NOTE: This bot assumes you use a new bot per ApePay deployment
 sm = StreamManager(os.environ["APEPAY_CONTRACT_ADDRESS"])
 
@@ -14,20 +14,20 @@ db: dict[int, Stream] = dict()
 # TODO: Migrate to `app.state.db` when feature becomes available
 
 
-@app.on_startup()
+@bot.on_startup()
 async def load_db(_):
     for stream in sm.active_streams():
         db[stream.id] = stream
 
 
-@sm.on_stream_created(app)
+@sm.on_stream_created(bot)
 async def grant_product(stream):
     db[stream.id] = stream
     print(f"provisioning products: {stream.products}")  # noqa: T001
     return stream.time_left
 
 
-@sm.on_stream_funded(app)
+@sm.on_stream_funded(bot)
 async def update_product_funding(stream):
     # NOTE: properties of stream have changed, you may not need to handle this, but typically you
     #       would want to update `stream.time_left` in db for use in user Stream life notifications
@@ -35,7 +35,7 @@ async def update_product_funding(stream):
     return stream.time_left
 
 
-@sm.on_stream_cancelled(app)
+@sm.on_stream_cancelled(bot)
 async def revoke_product(stream):
     print(f"unprovisioning product for {stream.creator}")  # noqa: T001
     db[stream.id] = None
